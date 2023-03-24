a1b61411745bbcf61e9847c52ffe87be6b211e51
zhouyu21 <zhouyu21@58.com>
2023-03-24 21:48:16 +0800

    add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,31 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "antd/dist/reset.css";
+import { Button, FloatButton, Image, notification } from "antd";
+import { StarOutlined } from "@ant-design/icons";
+import { BASE_URL } from "@/const";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  function openQ() {
+    notification.open({
+      message: <Image src={`${BASE_URL}/kele.jpg`} alt="" />,
+      icon: null,
+    });
+  }
+  return (
+    <>
+      <Component {...pageProps} />
+      <FloatButton.Group>
+        <FloatButton
+          icon={<StarOutlined />}
+          tooltip={
+            <Button type="link" onClick={openQ}>
+              <span style={{ color: "white" }}>请我喝阔啦</span>
+            </Button>
+          }
+        />
+        <FloatButton.BackTop />
+      </FloatButton.Group>
+    </>
+  );
 }
