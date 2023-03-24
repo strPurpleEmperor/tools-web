dbc151790960085e123bd5e8b62515face827b4c
zhouyu21 <zhouyu21@58.com>
2023-03-24 15:14:42 +0800

    feat: add

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/tools-web",
+  swcMinify: true,
+  assetPrefix: "./",
 };
 
 module.exports = nextConfig;
